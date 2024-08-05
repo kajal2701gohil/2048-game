b52c5757d11dae56d37d3c3e3428e12db266fd68
DESKTOP-J8KISRM\Admin <kajalgohil.devotee@gmail.com>
2024-08-05 15:19:18 +0530

    add final files

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+"use strict";
+
 const game = document.querySelector("#game");
 const fixNumbers = [2, 4];
 let matrix = [];
 let column = [];
+let noSpace = 0;
 
 for (let i = 1; i <= 4; i++) {
     game.innerHTML += `<div class="row row${i}"></div>`;
@@ -13,29 +16,23 @@ for (let i = 1; i <= 4; i++) {
     column = document.querySelectorAll(".col");
 };
 
-
 function getRandom(range) {
     return Math.floor(Math.random() * range);
 };
 
-
 function boxText() {
     let res = getRandom(matrix.length);
     let box = document.getElementById(`${[matrix[res]]}`);
-    let fullyBox = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
+    let fullBoxes = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
     if (box.textContent === "") {
         box.textContent = Number(fixNumbers[getRandom(fixNumbers.length)]);
     }
     else {
-        if (fullyBox.length !== matrix.length) {
+        if (fullBoxes.length !== matrix.length) {
             boxText();
         }
     }
-    // if (fullyBox.length === matrix.length - 1) {
-    //     document.querySelector("#info").textContent = "Game Over";
-    // }
 };
-
 
 function printNumbers() {
     for (let i = 0; i < fixNumbers.length; i++) {
@@ -45,12 +42,20 @@ function printNumbers() {
 
 printNumbers();
 
+function getContentBoxes() {
+    return matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
+};
+
+function moveBoxes(targetBoxes, changeBox) {
+    if (JSON.stringify(targetBoxes) !== JSON.stringify(changeBox)) {
+        boxText();
+    };
+};
 
 window.addEventListener("keydown", (e) => {
     if (e.code === "ArrowUp") {
-        console.log("up");
-        const targetBox = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
-        for (let x of targetBox) {
+        const targetBoxes = getContentBoxes();
+        for (let x of targetBoxes) {
             for (let i = x[0] - 1; i > 0; i--) {
                 let emptyBox = document.getElementById(`${[i, x[1]]}`);
                 let currentBox = document.getElementById(`${[i + 1, x[1]]}`);
@@ -62,19 +67,17 @@ window.addEventListener("keydown", (e) => {
                     emptyBox.textContent = Number(currentBox.textContent) + Number(emptyBox.textContent);
                     currentBox.textContent = "";
                     break;
-                }
-            }
-        }
+                };
+            };
+        };
+
         let changeBox = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
-        if (JSON.stringify(targetBox) !== JSON.stringify(changeBox)) {
-            boxText();
-        }
-    }
+        moveBoxes(targetBoxes, changeBox);
+    };
 
     if (e.code === "ArrowDown") {
-        console.log("down");
-        let targetBox = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
-        for (let x of targetBox.reverse()) {
+        const targetBoxes = getContentBoxes();
+        for (let x of targetBoxes.reverse()) {
             for (let i = x[0] + 1; i <= 4; i++) {
                 let emptyBox = document.getElementById(`${[i, x[1]]}`);
                 let currentBox = document.getElementById(`${[i - 1, x[1]]}`);
@@ -86,18 +89,17 @@ window.addEventListener("keydown", (e) => {
                     emptyBox.textContent = Number(currentBox.textContent) + Number(emptyBox.textContent);
                     currentBox.textContent = "";
                     break;
-                }
-            }
-        }
-        let changeBox = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
-        if (JSON.stringify(targetBox) !== JSON.stringify(changeBox.reverse())) {
-            boxText();
-        }
-    }
+                };
+            };
+        };
+
+        let changeBox = matrix.filter((x => document.getElementById(`${x}`).textContent != "")).reverse();
+        moveBoxes(targetBoxes, changeBox);
+    };
+
     if (e.code === "ArrowLeft") {
-        console.log("left");
-        let targetBox = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
-        for (let x of targetBox) {
+        const targetBoxes = getContentBoxes();
+        for (let x of targetBoxes) {
             for (let i = x[1] - 1; i > 0; i--) {
                 let emptyBox = document.getElementById(`${[x[0], i]}`);
                 let currentBox = document.getElementById(`${[x[0], i + 1]}`);
@@ -109,18 +111,17 @@ window.addEventListener("keydown", (e) => {
                     emptyBox.textContent = Number(currentBox.textContent) + Number(emptyBox.textContent);
                     currentBox.textContent = "";
                     break;
-                }
-            }
-        }
+                };
+            };
+        };
+
         let changeBox = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
-        if (JSON.stringify(targetBox) !== JSON.stringify(changeBox)) {
-            boxText();
-        }
-    }
+        moveBoxes(targetBoxes, changeBox);
+    };
+
     if (e.code === "ArrowRight") {
-        console.log("right");
-        let targetBox = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
-        for (let x of targetBox.reverse()) {
+        const targetBoxes = getContentBoxes();
+        for (let x of targetBoxes.reverse()) {
             for (let i = x[1] + 1; i <= 4; i++) {
                 let emptyBox = document.getElementById(`${[x[0], i]}`);
                 let currentBox = document.getElementById(`${[x[0], i - 1]}`);
@@ -132,13 +133,37 @@ window.addEventListener("keydown", (e) => {
                     emptyBox.textContent = Number(currentBox.textContent) + Number(emptyBox.textContent);
                     currentBox.textContent = "";
                     break;
-                }
-            }
-        }
-        let changeBox = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
-        if (JSON.stringify(targetBox) !== JSON.stringify(changeBox.reverse())) {
-            boxText();
-        }
-    }
-})
+                };
+            };
+        };
 
+        let changeBox = matrix.filter((x => document.getElementById(`${x}`).textContent != "")).reverse();
+        moveBoxes(targetBoxes, changeBox);
+    };
+
+    for (let x of column) {
+        if (x.textContent === "2048") {
+            document.querySelector("#info").textContent = "You Win!";
+        };
+    };
+
+    let fullBoxes = matrix.filter((x => document.getElementById(`${x}`).textContent != ""));
+    if (fullBoxes.length === column.length) {
+        for (let x of column) {
+            let [a, b] = x.id.split(",");
+            a = Number(a);
+            b = Number(b);
+            const siblings = [[a - 1, b], [a, b - 1], [a, b + 1], [a + 1, b]];
+            let noMatch = siblings.filter(y => (document.getElementById(`${y.toString()}`)?.textContent) == x.textContent);
+            if (noMatch.length === 0) {
+                noSpace++;
+            }
+            else {
+                noSpace = 0;
+            };
+        };
+        if (noSpace >= column.length) {
+            document.querySelector("#info").textContent = "Game Over!";
+        };
+    };
+});
